e441c09bbc9050d9a17ea844a23ce4937c84d56a
VirulD <waviruldesilva@gmail.com>
2025-07-08 19:29:47 +0530

    fixed some errors

--- android/app/src/main/java/com/medibookapp/MainApplication.kt
+++ android/app/src/main/java/com/medibookapp/MainApplication.kt
@@ -9,6 +9,7 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
+import com.facebook.react.soloader.OpenSourceMergedSoMapping
 
 class MainApplication : Application(), ReactApplication {
 
@@ -33,9 +34,9 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onCreate() {
     super.onCreate()
-    SoLoader.init(this, false)
+    SoLoader.init(this, OpenSourceMergedSoMapping)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-    }
-  }
+      load()
+  }
+  }
 } 
